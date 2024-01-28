b26777cd60cbe7b305693eb18d5f947ec3d18ae2
Afnan Ngathour Rahman <avnanrahman@gmail.com>
2024-01-28 10:31:47 +0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+python3 -m py_compile sources/add2vals.py sources/calc.py
+
+python3 -m pytest --verbose --junit-xml test-reports/results.xml sources/test_calc.py
+
+python3 -m PyInstaller --onefile sources/add2vals.py
