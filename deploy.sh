28349bdec492ab46c78a9183c84389aa7e292b3a
Afnan Ngathour Rahman <avnanrahman@gmail.com>
2024-01-28 10:33:06 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,6 @@
+cd /home/ubuntu/dicoding/deploy/simple-python-pyinstaller-app
+git pull
+
 python3 -m py_compile sources/add2vals.py sources/calc.py
 
 python3 -m pytest --verbose --junit-xml test-reports/results.xml sources/test_calc.py
